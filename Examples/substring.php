7ed8f88255fd32b6ce521c5ae0fc63db136776da
KyleTheHack3r <thepmteam@hotmail.com>
2015-10-06 11:26:04 +0100

    Create substring.php

---
+++ Examples/substring.php
@@ -0,0 +1,13 @@
+<?php
+namespace napespace;
+//use pm shit
+class Main extends PluginBase implements Listener{
+public function sub(){
+  $sting = "Hello World";
+  $s1 = $sting[-1]; // the last letter
+  $s2 = $sting[0:4]; // the world hello
+  $s3 = $string[6:10]; // the word world
+}
+
+}
+?>
