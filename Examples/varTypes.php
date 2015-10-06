b9214d7596900f01a22796d1eee328ffbc5eb324
KyleTheHack3r <thepmteam@hotmail.com>
2015-10-06 11:22:55 +0100

    Create varTypes.php

---
+++ Examples/varTypes.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace napespace;
+
+//use pm shit
+
+class Main extends PluginBase implements Listener{
+
+public function data(){
+
+$array = array("str1","str2","str3");
+$array2 = array(
+    "name" => "test",
+    "data" => "test",
+);
+$name = "Kyle";
+$version = 1;
+$float = 144.2;
+$boolean = 0;
+$x = true;
+
+}
+
+}
